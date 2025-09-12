99b0b0df8372beb1462c07a63054786fd09c8f11
Anton Fesenko <tohafesenkolovep@gmail.com>
2025-09-12 17:20:06 +0300

    add logger to polling OnFilesAddedOrUpdated event, add OnFilesAddedOrUpdated polling test, add SearchFiles action test

--- Apps.Dropbox/Webhooks/PollingList.cs
+++ Apps.Dropbox/Webhooks/PollingList.cs
@@ -9,116 +9,120 @@ using Blackbird.Applications.SDK.Blueprints;
 using Dropbox.Api;
 using Dropbox.Api.Files;
 
-namespace Apps.Dropbox.Webhooks
+namespace Apps.Dropbox.Webhooks;
+
+[PollingEventList("Files")]
+public class PollingList : BaseInvocable
 {
-    [PollingEventList("Files")]
-    public class PollingList : BaseInvocable
+    private readonly DropboxClient _dropboxClient;
+
+    public PollingList(InvocationContext invocationContext) : base(invocationContext)
     {
-        private readonly DropboxClient _dropboxClient;
+        _dropboxClient = DropboxClientFactory.CreateDropboxClient(invocationContext.AuthenticationCredentialsProviders);
+    }
 
-        public PollingList(InvocationContext invocationContext) : base(invocationContext)
-        {
-            _dropboxClient = DropboxClientFactory.CreateDropboxClient(invocationContext.AuthenticationCredentialsProviders);
-        }
+    [BlueprintEventDefinition(BlueprintEvent.FilesCreatedOrUpdated)]
+    [PollingEvent("On files updated", "Triggered when files are updated or new files are created")]
+    public async Task<PollingEventResponse<CursorMemory, ListResponse<FileDto>>> OnFilesAddedOrUpdated(
+        PollingEventRequest<CursorMemory> request,
+        [PollingEventParameter] ParentFolderInput folder
+        )
+    {
+        InvocationContext.Logger?.LogInformation("[Dropbox OnFilesAddedOrUpdated] Polling started", null);
 
-        [BlueprintEventDefinition(BlueprintEvent.FilesCreatedOrUpdated)]
-        [PollingEvent("On files updated", "Triggered when files are updated or new files are created")]
-        public async Task<PollingEventResponse<CursorMemory, ListResponse<FileDto>>> OnFilesAddedOrUpdated(
-            PollingEventRequest<CursorMemory> request,
-            [PollingEventParameter] ParentFolderInput folder
-            )
+        string parentFolderLowerPath = folder.ParentFolderLowerPath == "/" ? string.Empty : folder.ParentFolderLowerPath ?? string.Empty;
+        if (request.Memory == null)
         {
-            string parentFolderLowerPath = folder.ParentFolderLowerPath == "/" ? string.Empty : folder.ParentFolderLowerPath ?? string.Empty;
-            if (request.Memory == null)
-            {
-                return new()
-                {
-                    FlyBird = false,
-                    Memory = new CursorMemory() { Cursor = await GetCursor(parentFolderLowerPath) }
-                };
-            }
-            string newCursor=null;
-            var changedItems = await ErrorWrapper.WrapError(() =>
-                Task.FromResult(GetChangedItems(request.Memory.Cursor, out newCursor)));
-            var files = changedItems.Where(item => item.IsFile).ToList();
-            if(files.Count == 0)
-                return new()
-                {
-                    FlyBird = false,
-                    Memory = new CursorMemory() { Cursor = newCursor }
-                };
             return new()
             {
-                FlyBird = true,
-                Memory = new CursorMemory() { Cursor = newCursor },
-                Result = new ListResponse<FileDto> { Files = files.Select(file => new FileDto(file.AsFile)) }
+                FlyBird = false,
+                Memory = new CursorMemory() { Cursor = await GetCursor(parentFolderLowerPath) }
+            };
+        }
+        string newCursor = null;
+        var changedItems = await ErrorWrapper.WrapError(() =>
+            Task.FromResult(GetChangedItems(request.Memory.Cursor, out newCursor)));
+        var files = changedItems.Where(item => item.IsFile).ToList();
+
+        if (files.Count == 0)
+        {
+            InvocationContext.Logger?.LogError("[Dropbox OnFilesAddedOrUpdated] No files received", null);
+            return new()
+            {
+                FlyBird = false,
+                Memory = new CursorMemory() { Cursor = newCursor }
+            };
+        }
+        return new()
+        {
+            FlyBird = true,
+            Memory = new CursorMemory() { Cursor = newCursor },
+            Result = new ListResponse<FileDto> { Files = files.Select(file => new FileDto(file.AsFile)) }
+        };
+    }
+
+    [PollingEvent("On files deleted", "Triggered when files are deleted")]
+    public async Task<PollingEventResponse<CursorMemory, ListResponse<DeletedItemDto>>> OnFileDeleted(
+        PollingEventRequest<CursorMemory> request,
+        [PollingEventParameter] ParentFolderInput folder
+        )
+    {
+        string parentFolderLowerPath = folder.ParentFolderLowerPath == "/" ? string.Empty : folder.ParentFolderLowerPath ?? string.Empty;
+        if (request.Memory == null)
+        {
+            return new()
+            {
+                FlyBird = false,
+                Memory = new CursorMemory() { Cursor = await GetCursor(parentFolderLowerPath) }
             };
         }
 
-        [PollingEvent("On files deleted", "Triggered when files are deleted")]
-        public async Task<PollingEventResponse<CursorMemory, ListResponse<DeletedItemDto>>> OnFileDeleted(
-            PollingEventRequest<CursorMemory> request,
-            [PollingEventParameter] ParentFolderInput folder
-            )
-        {
-            string parentFolderLowerPath = folder.ParentFolderLowerPath == "/" ? string.Empty : folder.ParentFolderLowerPath ?? string.Empty;
-            if (request.Memory == null)
-            {
-                return new()
-                {
-                    FlyBird = false,
-                    Memory = new CursorMemory() { Cursor = await GetCursor(parentFolderLowerPath) }
-                };
-            }
-
-            string newCursor=null;
-            var changedItems = await ErrorWrapper.WrapError(() =>
-                Task.FromResult(GetChangedItems(request.Memory.Cursor, out newCursor))
-            );
-            var deletedFiles = changedItems.Where(item => item.IsDeleted).ToList();
-            if (deletedFiles.Count == 0)
-                return new()
-                {
-                    FlyBird = false,
-                    Memory = new CursorMemory() { Cursor = newCursor }
-                };
+        string newCursor=null;
+        var changedItems = await ErrorWrapper.WrapError(() =>
+            Task.FromResult(GetChangedItems(request.Memory.Cursor, out newCursor))
+        );
+        var deletedFiles = changedItems.Where(item => item.IsDeleted).ToList();
+        if (deletedFiles.Count == 0)
             return new()
             {
-                FlyBird = true,
-                Memory = new CursorMemory() { Cursor = newCursor },
-                Result = new ListResponse<DeletedItemDto> { Files = deletedFiles.Select(file => new DeletedItemDto(file.AsDeleted)) }
+                FlyBird = false,
+                Memory = new CursorMemory() { Cursor = newCursor }
             };
-        }
-
-        private async Task<string> GetCursor(string folderPath)
+        return new()
         {
-            var listFolderResult = await _dropboxClient.Files.ListFolderAsync(folderPath, recursive: true);
-            var cursor = listFolderResult.Cursor;
+            FlyBird = true,
+            Memory = new CursorMemory() { Cursor = newCursor },
+            Result = new ListResponse<DeletedItemDto> { Files = deletedFiles.Select(file => new DeletedItemDto(file.AsDeleted)) }
+        };
+    }
 
-            while (listFolderResult.HasMore)
-            {
-                listFolderResult = await _dropboxClient.Files.ListFolderContinueAsync(cursor);
-                cursor = listFolderResult.Cursor;
-            }
-            return cursor;
-        }
+    private async Task<string> GetCursor(string folderPath)
+    {
+        var listFolderResult = await _dropboxClient.Files.ListFolderAsync(folderPath, recursive: true);
+        var cursor = listFolderResult.Cursor;
 
-        private List<Metadata> GetChangedItems(string cursor, out string newCursor)
+        while (listFolderResult.HasMore)
         {
-            var changedItems = new List<Metadata>();
-            var listFolderResult = _dropboxClient.Files.ListFolderContinueAsync(cursor).Result;
+            listFolderResult = await _dropboxClient.Files.ListFolderContinueAsync(cursor);
+            cursor = listFolderResult.Cursor;
+        }
+        return cursor;
+    }
+
+    private List<Metadata> GetChangedItems(string cursor, out string newCursor)
+    {
+        var changedItems = new List<Metadata>();
+        var listFolderResult = _dropboxClient.Files.ListFolderContinueAsync(cursor).Result;
+        newCursor = listFolderResult.Cursor;
+        changedItems.AddRange(listFolderResult.Entries);
+
+        while (listFolderResult.HasMore)
+        {
+            listFolderResult = _dropboxClient.Files.ListFolderContinueAsync(newCursor).Result;
             newCursor = listFolderResult.Cursor;
             changedItems.AddRange(listFolderResult.Entries);
-
-            while (listFolderResult.HasMore)
-            {
-                listFolderResult = _dropboxClient.Files.ListFolderContinueAsync(newCursor).Result;
-                newCursor = listFolderResult.Cursor;
-                changedItems.AddRange(listFolderResult.Entries);
-            }
-
-            return changedItems;
         }
 
+        return changedItems;
     }
 }
---
+++ Tests.Dropbox/PollingTests.cs
@@ -0,0 +1,35 @@
+﻿using Tests.Dropbox.Base;
+using Apps.Dropbox.Webhooks;
+using Apps.Dropbox.Webhooks.Inputs;
+using Apps.Dropbox.Webhooks.Polling.Memory;
+using Blackbird.Applications.Sdk.Common.Polling;
+
+namespace Tests.Dropbox;
+
+[TestClass]
+public class PollingTests : TestBase
+{
+    [TestMethod]
+    public async Task OnFilesAddedOrUpdated_IsSuccess()
+    {
+        // Arrange
+        var action = new PollingList(InvocationContext);
+        string cursorString = "AATR40MfMqwNhW6OXFKyn0J3X1DgEN6tXMdXdFAfhvxKU0Ar5tj3xVirRzJOrm5rXX4bP0ehw3LfVoqtwKYW4myRyGagJDpl1B73BSjPlWgVyFZcSrhuhcv4VrriCrn8BCGiEAOjsdqLmUZinY1M6J7KsfFE78FWKXZVfk1pbyAgUZLipwyjxROTrbcXHhOSwtSVwpusdAG3V_SyEe9tLbAb9ujUrdNg3QbibKnnrWgNaA";
+
+        var cursor = new CursorMemory { Cursor = cursorString };
+        var request = new PollingEventRequest<CursorMemory> { Memory = cursor };
+        var folder = new ParentFolderInput { ParentFolderLowerPath = "/z86s34d0/export" };
+
+        // Act
+        var result = await action.OnFilesAddedOrUpdated(request, folder);
+
+        // Assert
+        Console.WriteLine("Changed files:");
+        foreach (var file in result.Result.Files)
+        {
+            Console.WriteLine(file.Name);
+        }
+
+        Assert.IsNotNull(result);
+    }
+}
