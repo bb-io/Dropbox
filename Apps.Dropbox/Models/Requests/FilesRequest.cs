3c66d7670573be87075c1c322a5ecfc7a50072b3
Anton Fesenko <tohafesenkolovep@gmail.com>
2025-09-12 18:35:10 +0300

    add update time filter to GetFilesListByPath action, update test

--- Apps.Dropbox/Models/Requests/FilesRequest.cs
+++ Apps.Dropbox/Models/Requests/FilesRequest.cs
@@ -1,11 +1,17 @@
 ﻿using Apps.Dropbox.DataSourceHandlers;
+using Blackbird.Applications.Sdk.Common;
 using Blackbird.Applications.Sdk.Common.Dynamic;
 
-namespace Apps.Dropbox.Models.Requests
+namespace Apps.Dropbox.Models.Requests;
+
+public class FilesRequest
 {
-    public class FilesRequest
-    {
-        [DataSource(typeof(FolderDataSourceHandler))]
-        public string Path { get; set; }
-    }
+    [DataSource(typeof(FolderDataSourceHandler))]
+    public string Path { get; set; }
+
+    [Display("Modified after")]
+    public DateTime? ModifiedAfter { get; set; }
+
+    [Display("Modified before")]
+    public DateTime? ModifiedBefore { get; set; }
 }
